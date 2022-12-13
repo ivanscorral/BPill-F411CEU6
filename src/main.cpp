efa8c6b03a0554587509461f75f10707ea002ed4
Iván Sánchez <isanchezc@kabel.es>
2022-12-13 17:32:24 +0100

    Adjusted error compensation to current instead of voltage

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 #define RESISTOR 0.10
 #define GAIN_ERROR_CORRECTION 1.01
 
-void setup() {
+// TODO: Add voltage divider and resistor calculation functions
+
+void setup() { 
   // put your setup code here, to run once:
   SerialUSB.begin(115200);
   while (!SerialUSB);;
@@ -15,37 +17,37 @@ void setup() {
 }
 double adcToVoltageMv(uint16_t adc) {
   double voltage = adc * 3300 / 4095.0;
-  if (voltage < 200) {
-    voltage -= 10;
-  }if (voltage < 50) {
-    voltage -= 20;
-   
-  }
-   if (voltage < 0) {
-      voltage = 0;
-    }
+
   return voltage;
 }
 
 double voltageToCurrentMa(double voltage) {
-  SerialUSB.print("voltage: ");
-  SerialUSB.println(voltage);
-  return voltage * DIVIDER * RESISTOR * 20;
+  double current = voltage * DIVIDER * RESISTOR * 20;
+  if (current < 200) {
+    current -= 10;
+  }if (current < 100) {
+     current -= 0;
+   
+  }
+   if (current < 0) {
+      current = 0;
+    }
+  return current;
 }
 
-double avgAdc(uint8_t pin, uint8_t samples) {
+double avgAdc(uint8_t pin, uint16_t samples) {
   double avg = 0.0;
   for (uint8_t i = 0; i < samples; i++) {
     avg += adcToVoltageMv(analogRead(pin)) / GAIN_ERROR_CORRECTION;
-    delay(5);
+    delay(1);
   }
-  avg = avg / samples;
+  avg = avg / samples - 6.0;
   return avg;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  double val = avgAdc(A0, 64);
+  double val = avgAdc(A0, 128);
   SerialUSB.print(val, 4);
   SerialUSB.print(" mV, ");
   SerialUSB.print(voltageToCurrentMa(val), 4);
